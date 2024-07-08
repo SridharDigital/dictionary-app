b253ecc1d550ce6f1938b92d767831704390efd1
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2024-07-08 13:20:40 +0530

    Setup React and Dictionary app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const dictionary = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [dispalyDefinition, setDispalyDefinition] = useState("");
+
+  const findWord = (e) => {
+    e.preventDefault();
+    const item = dictionary.find(
+      (item) =>
+        item.word.toLocaleLowerCase() === searchValue.toLocaleLowerCase()
+    );
+    console.log(item);
+    setDispalyDefinition(item?.meaning);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <h1 className="heading">Dictionary App</h1>
+      <form onSubmit={findWord}>
+        <input
+          type="search"
+          placeholder="Search for a word..."
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <h2>Definition:</h2>
+      <p>
+        {dispalyDefinition
+          ? dispalyDefinition
+          : "Word not found in the dictionary"}
+      </p>
     </div>
   );
-}
+};
 
 export default App;
